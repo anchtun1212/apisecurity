1789b0b19f27ba31315962bda4c41e37777e020d
anchtun1212 <med.aymen.charrada@gmail.com>
2023-09-21 16:59:21 +0300

    add fixed stored procedure

--- apisecurity/src/main/java/com/anchtun/apisecurity/api/filter/SqlInjectionFilter.java
+++ apisecurity/src/main/java/com/anchtun/apisecurity/api/filter/SqlInjectionFilter.java
@@ -21,7 +21,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+//@Component
 public class SqlInjectionFilter extends OncePerRequestFilter {
 
 	// So we need to specify the dangerous regular expression.
