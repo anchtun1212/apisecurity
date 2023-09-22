23f1844b6bbcb7004de9c6f1c0aa43cb99a74168
anchtun1212 <med.aymen.charrada@gmail.com>
2023-09-22 06:25:11 +0300

    create_limited_user.sql

--- apisecurity/src/main/java/com/anchtun/apisecurity/api/server/sqlinjection/JpaCustomerSafeApi.java
+++ apisecurity/src/main/java/com/anchtun/apisecurity/api/server/sqlinjection/JpaCustomerSafeApi.java
@@ -5,45 +5,32 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anchtun.apisecurity.entity.JpaCustomer;
 import com.anchtun.apisecurity.repository.JpaCustomerCrudRepository;
-import com.anchtun.apisecurity.repository.JpaCustomerDangerDAO;
+import com.anchtun.apisecurity.repository.JpaCustomerSafeDAO;
 
 @RestController
-@RequestMapping("/api/sqlinjection/danger/v2")
-public class JpaCustomerDangerApi {
+@RequestMapping("/api/sqlinjection/safe/v2")
+public class JpaCustomerSafeApi {
 
 	@Autowired
 	private JpaCustomerCrudRepository repository;
 
 	@Autowired
-	private JpaCustomerDangerDAO dao;
+	private JpaCustomerSafeDAO dao;
 
 	@GetMapping(value = "/customer/{email}")
 	public JpaCustomer findCustomerByEmail(@PathVariable(required = true, name = "email") String email) {
-		var queryResult = repository.findByEmail(email);
-
-		if (queryResult != null && !queryResult.isEmpty()) {
-			return queryResult.get(0);
-		}
-
-		return null;
+		return repository.findByEmail(email).get(0);
 	}
 
 	@GetMapping(value = "/customer")
 	public List<JpaCustomer> findCustomersByGender(@RequestParam(required = true, name = "genderCode") String genderCode) {
 		return dao.findByGender(genderCode);
-	}
-
-	@PostMapping(value = "/customer")
-	public void createCustomer(@RequestBody(required = true) JpaCustomer newCustomer) {
-		repository.save(newCustomer);
 	}
 
 }
---
+++ apisecurity/src/main/java/com/anchtun/apisecurity/repository/JpaCustomerSafeDAO.java
@@ -0,0 +1,25 @@
+package com.anchtun.apisecurity.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.anchtun.apisecurity.entity.JpaCustomer;
+
+import jakarta.persistence.EntityManager;
+
+@Component
+public class JpaCustomerSafeDAO {
+
+	@Autowired
+	private EntityManager entityManager;
+
+	public List<JpaCustomer> findByGender(String gender) {
+		var jpql = "FROM JpaCustomer WHERE gender = :gender";
+		var query = entityManager.createQuery(jpql, JpaCustomer.class).setParameter("gender", gender);
+
+		return query.getResultList();
+	}
+
+}
