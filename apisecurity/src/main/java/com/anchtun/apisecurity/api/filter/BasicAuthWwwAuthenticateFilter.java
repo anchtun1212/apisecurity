e37a2ec21ab6ca328d9abee575c999c2b6a77389
anchtun1212 <med.aymen.charrada@gmail.com>
2023-09-26 23:29:24 +0300

    WWW_AUTHENTICATE

--- apisecurity/src/main/java/com/anchtun/apisecurity/api/filter/BasicAuthWwwAuthenticateFilter.java
+++ apisecurity/src/main/java/com/anchtun/apisecurity/api/filter/BasicAuthWwwAuthenticateFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -19,15 +20,16 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class BasicAuthFilter extends OncePerRequestFilter {
+//@Component
+public class BasicAuthWwwAuthenticateFilter extends OncePerRequestFilter {
 
 	private BasicAuthUserRepository basicAuthUserRepository;
 
-	public BasicAuthFilter(BasicAuthUserRepository basicAuthUserRepository) {
+	public BasicAuthWwwAuthenticateFilter(BasicAuthUserRepository basicAuthUserRepository) {
 		this.basicAuthUserRepository = basicAuthUserRepository;
 	}
 
-	private boolean isValidBasicAuth(String basicAuthString) throws Exception {
+	private boolean isValidBasicAuth(String basicAuthString) {
 		try {
 			var encodedAuthorizationString = StringUtils.substring(basicAuthString, "Basic".length()).trim();
 			var plainAuthorizationString = EncodeDecodeUtil.decodeBase64(encodedAuthorizationString);
@@ -47,7 +49,7 @@ public class BasicAuthFilter extends OncePerRequestFilter {
 			return false;
 		}
 	}
-	
+
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws ServletException, IOException {
@@ -59,6 +61,7 @@ public class BasicAuthFilter extends OncePerRequestFilter {
 			} else {
 				response.setStatus(HttpStatus.UNAUTHORIZED.value());
 				response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+				response.addHeader(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"Need credential to access API\"");
 				PrintWriter writer = response.getWriter();
 				writer.print("{\"message\":\"Invalid credential\"}");
 				return;
@@ -67,5 +70,4 @@ public class BasicAuthFilter extends OncePerRequestFilter {
 			e.printStackTrace();
 		}
 	}
-
 }
