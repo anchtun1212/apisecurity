d423127c61324d5314534388313ede25d49c4895
anchtun1212 <med.aymen.charrada@gmail.com>
2023-09-26 22:23:15 +0300

    hmac

--- apisecurity/src/main/java/com/anchtun/apisecurity/api/filter/config/HmacFilterConfig.java
+++ apisecurity/src/main/java/com/anchtun/apisecurity/api/filter/config/HmacFilterConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.anchtun.apisecurity.api.filter.HmacFilter;
 
-@Configuration
+//@Configuration
 public class HmacFilterConfig {
 
 	@Bean
