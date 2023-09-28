23af15aeea33251552eb0ea247b91a8ae960d355
anchtun1212 <med.aymen.charrada@gmail.com>
2023-09-28 09:07:59 +0300

    LOG

---
+++ apisecurity/src/main/java/com/anchtun/apisecurity/api/filter/config/RequestLogFilterConfig.java
@@ -0,0 +1,21 @@
+package com.anchtun.apisecurity.api.filter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+// We will use Loggly
+//@Configuration
+public class RequestLogFilterConfig {
+
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		var filter = new CommonsRequestLoggingFilter();
+
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setIncludeHeaders(true);
+
+		return filter;
+	}
+
+}
