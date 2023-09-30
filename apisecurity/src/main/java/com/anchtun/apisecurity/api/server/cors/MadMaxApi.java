e913191de8d37fb997ac460690f8dfc010811176
anchtun1212 <med.aymen.charrada@gmail.com>
2023-09-30 16:36:40 +0300

    CORS

--- apisecurity/src/main/java/com/anchtun/apisecurity/api/server/cors/MadMaxApi.java
+++ apisecurity/src/main/java/com/anchtun/apisecurity/api/server/cors/MadMaxApi.java
@@ -17,7 +17,7 @@ public class MadMaxApi {
 	}
 
 	@PostMapping(value = "/max", produces = MediaType.TEXT_PLAIN_VALUE)
-//	@CrossOrigin(origins = { "http://127.0.0.1:8080", "http://192.168.152.56:8098" })
+//	@CrossOrigin(origins = { "http://127.0.0.1:8098", "http://192.168.152.56:8098" })
 	private String max() {
 		return "Max";
 	}
